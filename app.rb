8260f714f1d5b8cf7ccaf9af9635a510e54181ad
Anders Janmyr <anders@janmyr.com>
2010-10-20 09:02:35 +0200

    moved from presentation

---
+++ app.rb
@@ -0,0 +1,60 @@
+# encoding: UTF-8
+require 'rubygems'
+require 'sinatra'
+require 'json'
+
+set :public, "./public"
+
+configure(:development) do |c|
+  require "sinatra/reloader"
+  c.also_reload "*.rb"
+end
+
+enable :run
+
+get "/" do
+  redirect 'index.html'
+end
+
+$quiz_counter = 0
+$quizzes = {};
+
+post "/quiz" do
+  $quiz_counter += 1
+  $quizzes[$quiz_counter] = rand(3) + 1
+  p $quizzes
+  $quiz_counter.to_s
+end
+
+
+put "/quiz/:quiz/select/:door" do |quiz, door|
+  answer = $quizzes[quiz.to_i]
+  doors = [1,2,3]
+  doors.delete(door.to_i)
+  doors.delete(answer)
+  p doors
+  doors[rand(doors.length + 1) - 1].to_s
+end
+
+$stick_count = 0
+$stick_correct_count = 0
+$switch_count = 0
+$switch_correct_count = 0
+put "/quiz/:quiz/:stick_or_switch/:door" do |quiz, stick_or_switch, door|
+  answer = $quizzes[quiz.to_i]
+  correct = answer == door.to_i
+  if stick_or_switch == 'stick'
+    $stick_count += 1
+    $stick_correct_count += 1 if correct
+  else
+    $switch_count += 1
+    $switch_correct_count += 1 if correct
+  end
+  {
+    :answer => answer,
+    :correct => correct,
+    :stick_or_switch => stick_or_switch,
+    :stick => [$stick_correct_count, $stick_count],
+    :switch => [$switch_correct_count, $switch_count]
+  }.to_json
+end
---
+++ public/app.js
@@ -0,0 +1,123 @@
+$(function() {
+
+  $.ajaxSetup({
+    dataType: 'text',
+    error: function(xhr, status, error) {
+      info('Error! ' +  ( error ? error : xhr.status ));
+    }
+  });
+
+  function info(text) {
+    $('#info-text').text(text);
+  }
+
+
+  $('#start').click(function() {
+    info("New quiz started");
+    $('#start').attr('disabled', 'disabled');
+    postQuiz();
+  });
+
+  function postQuiz() {
+    $.ajax({
+      type: 'POST',
+      url: '/quiz',
+      success: function(token) {
+        saveToken(token);
+        enableDoors();
+      }
+    });
+  }
+
+  var currentToken;
+  function saveToken(token) {
+    currentToken = token;
+  }
+
+  function enableDoors() {
+    info("Select a door!");
+    $('#doors li')
+      .addClass('enabled')
+      .removeClass('selected removed tapir')
+      .hover(
+        function() {$(this).addClass('hover')},
+        function() {$(this).removeClass('hover')})
+      .click(function() {
+        var door = $(this)
+          .removeClass('enabled')
+          .addClass('selected')
+          .attr('data-id');
+        $('#doors li')
+          .unbind('click mouseenter mouseleave')
+        putSelected(door);
+      });
+  }
+
+  function putSelected(door) {
+    $.ajax({
+      type: 'PUT',
+      url: '/quiz/' + currentToken + '/select/' + door,
+      success: function(data) {
+        info("Stick or Switch!");
+        $('#door-' + data)
+          .removeClass('enabled')
+          .addClass('removed')
+          .find('div').text('Removed');
+        $('#stick-switch button').removeAttr('disabled');
+      }
+    });
+  }
+
+  $('#stick-switch button').click(function(e) {
+    e.preventDefault();
+    var value = $(this).attr('data-id');
+    putStickOrSwitch(value);
+  });
+
+  function putStickOrSwitch(value) {
+    var door = (value == 'stick')
+      ? $('#doors li.selected').attr('data-id')
+      : $('#doors li.enabled').attr('data-id');
+    $.ajax({
+      type: 'PUT',
+      dataType: 'json',
+      url: '/quiz/' + currentToken + '/' + value + '/' + door,
+      success: function(data) {
+        $('#door-' + data.answer)
+          .removeClass('selected enabled removed')
+          .addClass('tapir');
+        infoResult(data);
+        updateStats(data);
+        resetButtons();
+      }
+    });
+  }
+
+  function infoResult(data) {
+    var right_or_wrong = data.correct ? ' right!' : ' wrong!';
+    var message =
+      (data.stick_or_switch == 'stick'
+        ? 'You stick, and you are'
+        : 'You switcharoo, you are')
+      + right_or_wrong;
+    info(message);
+  }
+
+  function updateStats(data) {
+    updateStat('stick', data);
+    updateStat('switch', data);
+  }
+
+  function updateStat(kind, data) {
+    var arr = data[kind];
+    var percentage = parseInt(arr[0] / arr[1] * 100);
+    $('#' + kind + '-count').text(arr[0] + '/' + arr[1] + ' = ' + percentage + '%');
+  }
+
+  function resetButtons() {
+    $('#start').removeAttr('disabled');
+    $('#stick-switch button').attr('disabled', 'disabled');
+  }
+
+
+});
